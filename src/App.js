19a5cc0afaba456693069ef31510d2d109218545
athar05 <atharshakeel96@gmail.com>
2022-05-19 00:42:03 +0530

    Added Routes
    Added routes for Home, Instruction, Quiz and Results pages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Home from "./pages/Home";
 import Rules from "./pages/Rules";
 import Result from "./pages/Result";
 import Quiz from "./pages/Quiz";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Rules />
-      <Result />
-      <Quiz />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rules" element={<Rules />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/Quiz" element={<Quiz />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/banner.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function banner() {
+  return <div>banner</div>;
+}
+
+export default banner;
