853756847113d3ccb899c4d83849133b097a24a6
athar05 <atharshakeel96@gmail.com>
2022-05-20 21:45:27 +0530

    Created a separate file for routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import "./App.css";
-import Home from "./pages/Home";
-import Rules from "./pages/Rules";
-import Result from "./pages/Result";
-import Quiz from "./pages/Quiz";
-import { Routes, Route } from "react-router-dom";
+import RoutesPages from "./components/RoutesPages";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rules" element={<Rules />} />
-        <Route path="/result" element={<Result />} />
-        <Route path="/Quiz" element={<Quiz />} />
-      </Routes>
+      <RoutesPages />
     </div>
   );
 }
