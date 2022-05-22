8248ede5be89b968dfec67857534715c63eb2d24
athar05 <atharshakeel96@gmail.com>
2022-05-22 23:52:12 +0530

    Added categories

---
+++ src/Data/Categories.js
@@ -0,0 +1,18 @@
+const Categories = [
+  {
+    category: "General Knowledge",
+    value: 9,
+    img: "/assets/images/quiz-general-knowledge",
+  },
+  { category: "Books", value: 10, img: "/assets/images/quiz-books" },
+  { category: "Films", value: 11, img: "/assets/images/quiz-movies" },
+  { category: "Music", value: 12, img: "/assets/images/quiz-music" },
+  {
+    category: "Science and Nature",
+    value: 17,
+    img: "/assets/images/quiz-science",
+  },
+  { category: "Sports", value: 21, img: "/assets/images/quiz-sports" },
+];
+
+export default Categories;
