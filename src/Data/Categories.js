f1befa4c851cb3b3d34b62695367caeb2aa4bcc9
athar05 <atharshakeel96@gmail.com>
2022-05-22 23:53:36 +0530

    Hotfix: categories file

--- src/Data/Categories.js
+++ src/Data/Categories.js
@@ -2,17 +2,17 @@ const Categories = [
   {
     category: "General Knowledge",
     value: 9,
-    img: "/assets/images/quiz-general-knowledge",
+    img: "/assets/images/quiz-general-knowledge.svg",
   },
-  { category: "Books", value: 10, img: "/assets/images/quiz-books" },
-  { category: "Films", value: 11, img: "/assets/images/quiz-movies" },
-  { category: "Music", value: 12, img: "/assets/images/quiz-music" },
+  { category: "Books", value: 10, img: "/assets/images/quiz-books.svg" },
+  { category: "Films", value: 11, img: "/assets/images/quiz-movies.svg" },
+  { category: "Music", value: 12, img: "/assets/images/quiz-music.svg" },
   {
     category: "Science and Nature",
     value: 17,
-    img: "/assets/images/quiz-science",
+    img: "/assets/images/quiz-science.svg",
   },
-  { category: "Sports", value: 21, img: "/assets/images/quiz-sports" },
+  { category: "Sports", value: 21, img: "/assets/images/quiz-sports.svg" },
 ];
 
 export default Categories;
