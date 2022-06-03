afbb4e506362a7e1af5b6aa582ac4054e0cba206
athar05 <atharshakeel96@gmail.com>
2022-06-03 17:43:53 +0530

    Added images, text and button with link

---
+++ src/components/Rules-Text/RulesText.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function RulesText() {
+  return (
+    <>
+      <div className="quiz-banner">
+        <img
+          src="/assets/images/minion-all-the-best.jpg"
+          alt=""
+          className="rules-banner"
+        />
+      </div>
+      <ol>
+        <li>
+          You are given 4 options for each question, choose 1 option from given
+          options.
+        </li>
+        <li> For each question youll get 1 points</li>
+        <li> No marks will be deducted for wrong answers</li>
+        <li> There will be total 10 questions</li>
+        <li>
+          For passing the quiz you have to give at-least answer 4 questions
+          correctly
+        </li>
+      </ol>
+    </>
+  );
+}
+
+export default RulesText;
