a11d6692a7104856f283f8ca5cb51f684d4c63b8
Athar Shakeel <82315023+athar05@users.noreply.github.com>
2022-10-06 13:22:48 +0530

    Added tests for components+changed result page UI

--- src/components/Result-body/ResultBody.js
+++ src/components/Result-body/ResultBody.js
@@ -7,30 +7,30 @@ function ResultBody() {
   const { score } = useContext(ScoreContext);
   return (
     <div className="result-body">
-      <h1>Result</h1>
+      <h2>Result</h2>
       {result.map((obj) => (
-        <div className="result-question-body">
+        <div>
           {obj.isCorrect ? (
-            <div>
-              <h3>{obj.question}</h3>
-              <div className="correct result-option">
-                <h4>Correct Answer: {obj.correct_answer}</h4>
+            <div className="result-question-body">
+              <h5>Q. {obj.question}</h5>
+              <div className="result-option">
+                <h6>Your Answer: {obj.correct_answer} ✅</h6>
               </div>
             </div>
           ) : (
             <div className="result-question-body">
-              <h3>{obj.question}</h3>
-              <div className="correct result-option">
-                <h4>Correct Answer: {obj.correct_answer}</h4>
+              <h5>Q. {obj.question}</h5>
+              <div className="result-option">
+                <h6>Your Answer: {obj.optionSelected} ❌</h6>
               </div>
-              <div className="wrong result-option">
-                <h4>Your Answer: {obj.optionSelected}</h4>
+              <div className="result-option">
+                <h6>Correct Answer: {obj.correct_answer} ✅</h6>
               </div>
             </div>
           )}
         </div>
       ))}
-      <h1 style={{ textAlign: "center" }}>📈 Your Score: {score}</h1>
+      <h3 style={{ textAlign: "center" }}>📈 Your Score: {score}</h3>
     </div>
   );
 }
